3beecd89a17ef48b10b9a89ef28de4e3157ad9c1
vukea <123366393+vukea@users.noreply.github.com>
2025-09-07 14:27:22 +0200

    Update __init__.py

--- maya_scripts/nodes/__init__.py
+++ maya_scripts/nodes/__init__.py
@@ -1,11 +1,3 @@
 """
 nodes/__init__.py
-Registry for supported nodes.
 """
-from .image_texture import ImageTextureNode
-from .principled_bsdf import PrincipledBSDFNode
-
-registry = {
-    "Image Texture": ImageTextureNode,
-    "Principled BSDF": PrincipledBSDFNode,
-}
